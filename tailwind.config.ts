b5c08a2eacbfc1883334c4dd894c566931e7b059
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-08 15:48:55 +0300

    Add shadcn/ui setup with a button.tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
