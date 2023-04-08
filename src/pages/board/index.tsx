5aae7a2eb9704ce79e824e8f21bcb4b435b92755
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-08 23:16:51 +0300

    Adjust board page visually

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,8 +1,8 @@
+import { ListIcon } from "lucide-react";
 import { type NextPage } from "next";
 import { useState } from "react";
 import { ItemsList } from "~/components/itemlist";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
 import { api } from "~/utils/api";
 
 const BoardPage: NextPage = () => {
@@ -17,27 +17,39 @@ const BoardPage: NextPage = () => {
   }
 
   return (
-    <>
-      <main className="flex min-h-screen flex-row items-center justify-center bg-slate-900">
-        <div className="ml-10 flex w-36 flex-col space-y-4">
-          {lists?.map((list) => (
-            <Button
-              key={list.id}
-              variant={currentLists.includes(list.id) ? "subtle" : "ghost"}
-              size="sm"
-              className="w-full justify-start"
-              onClick={() => setCurrentLists([list.id])}
-            >
-              {list.title}
-            </Button>
-          ))}
+    <main className="flex min-h-screen flex-row bg-gradient-to-b from-rose-500 to-indigo-700">
+      <div className="mx-3 mt-3 grid h-[97vh] w-full grid-cols-4 rounded-md bg-slate-900 p-5 xl:grid-cols-5 ">
+        <aside className="space-y-5 pr-10">
+          <h1 className="align-center mb-2 flex flex-row px-2 text-2xl font-extrabold tracking-tight">
+            CONSUME
+          </h1>
+          <div>
+            <h2 className="align-center mb-2 flex flex-row px-2 text-lg font-semibold tracking-tight">
+              Lists
+            </h2>
+            <div className="space-y-2">
+              {lists?.map((list) => (
+                <Button
+                  key={list.id}
+                  variant={currentLists.includes(list.id) ? "subtle" : "ghost"}
+                  size="sm"
+                  className="w-full justify-start"
+                  onClick={() => setCurrentLists([list.id])}
+                >
+                  <ListIcon className="mr-2 h-4 w-4" />
+                  {list.title}
+                </Button>
+              ))}
+            </div>
+          </div>
+        </aside>
+        <div className="col-span-3 border-l border-slate-500 xl:col-span-4">
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            {currentLists[0] && <ItemsList listId={currentLists[0]} />}
+          </div>
         </div>
-        <div className="flex flex-col"></div>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {currentLists[0] && <ItemsList listId={currentLists[0]} />}
-        </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
