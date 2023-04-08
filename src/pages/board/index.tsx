5f3a0b29adef5a6a29bf41bfcc9fd1e30c542c8c
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-08 16:09:59 +0300

    Edit initial template to reroute to empty board page

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,20 @@
+import { type NextPage } from "next";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+import { api } from "~/utils/api";
+
+const BoardPage: NextPage = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <h1>Consume board</h1>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <Button>Create Item</Button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default BoardPage;
