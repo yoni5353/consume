ecea48a321404373604b8837b3a7c91b2c7f5c55
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-09 19:44:10 +0300

    Add selection logic (not ideal yet)

--- src/components/itemcard.tsx
+++ src/components/itemcard.tsx
@@ -19,21 +19,33 @@ import {
   ContextMenuItem,
   ContextMenuSeparator,
 } from "./ui/context-menu";
+import { cn } from "~/utils/ui/cn";
 
 const listsMock = ["toread", "towatch"];
 
 export function ItemCard({
   item,
   onDelete,
+  onClick,
+  selected,
 }: {
   item: Item;
   onDelete: () => void;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  selected: boolean;
 }) {
   return (
     <ContextMenu>
       <ContextMenuTrigger>
         <div className="flex flex-col gap-5">
-          <Button className="flex w-[50vw] justify-between gap-5 rounded-md bg-slate-900 px-5 py-1 text-left dark:bg-slate-800 dark:text-slate-200 dark:hover:bg-slate-700 dark:hover:text-slate-100">
+          <Button
+            onClick={onClick}
+            className={cn(
+              "flex w-[50vw] justify-between gap-5 rounded-md bg-slate-900 px-5 py-1 text-left",
+              !selected &&
+                "dark:bg-slate-800 dark:text-slate-200 dark:hover:bg-slate-700 dark:hover:text-slate-100"
+            )}
+          >
             <div className="space-x-3">
               <span className="font-medium">{item.title}</span>
               <span className="text-slate-400">{item.description}</span>
--- src/components/itemlist.tsx
+++ src/components/itemlist.tsx
@@ -4,9 +4,12 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function ItemsList({ listId }: { listId: string }) {
   const { register, handleSubmit, reset } = useForm<{ itemTitle: string }>();
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [lastSelectedItem, setLastSelectedItem] = useState<string>();
 
   const onCreateItem: SubmitHandler<{ itemTitle: string }> = (data) => {
     addItem({
@@ -20,14 +23,59 @@ export function ItemsList({ listId }: { listId: string }) {
     onSuccess: () => refetch(),
   });
 
-  const { data: listWithItems, refetch } =
-    api.lists.getWithItems.useQuery(listId);
+  const { data: listWithItems, refetch } = api.lists.getWithItems.useQuery(
+    listId,
+    {
+      onSuccess: () => {
+        if (listWithItems && listWithItems.items[0]) {
+          setLastSelectedItem(listWithItems.items[0].itemId);
+        }
+      },
+    }
+  );
 
   const { mutate: addItem } = api.lists.createItemInList.useMutation({
     onSuccess: () => refetch(),
   });
 
   const items = listWithItems?.items;
+
+  if (!items) return null;
+
+  const onCardClick = (e: React.MouseEvent, itemId: string) => {
+    const newSelectedItem = itemId;
+    if (e.ctrlKey) {
+      setSelectedItems((prev) => {
+        if (prev.includes(itemId)) {
+          return prev.filter((id) => id !== itemId);
+        } else {
+          return [...prev, itemId];
+        }
+      });
+    } else if (e.shiftKey) {
+      if (lastSelectedItem) {
+        const index = items.findIndex((item) => item.itemId === itemId);
+        const firstIndex = items.findIndex(
+          (item) => item.itemId === lastSelectedItem
+        );
+        if (~index && ~firstIndex) {
+          const start = Math.min(index, firstIndex);
+          const end = Math.max(index, firstIndex);
+          const newSelectedItems = items
+            .slice(start, end + 1)
+            .map((item) => item.itemId);
+          setSelectedItems((prev) => {
+            return [...prev, ...newSelectedItems];
+          });
+        }
+      }
+    } else {
+      setSelectedItems([itemId]);
+    }
+    if (!e.shiftKey) {
+      setLastSelectedItem(newSelectedItem);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-3">
@@ -41,11 +89,15 @@ export function ItemsList({ listId }: { listId: string }) {
         </Button>
       </form>
       {items?.map((item) => (
-        <ItemCard
-          item={item.item}
-          key={item.itemId}
-          onDelete={() => deleteItem(item.itemId)}
-        />
+        <div>
+          <ItemCard
+            item={item.item}
+            key={item.itemId}
+            selected={selectedItems.includes(item.itemId)}
+            onDelete={() => deleteItem(item.itemId)}
+            onClick={(e) => onCardClick(e, item.itemId)}
+          />
+        </div>
       ))}
     </div>
   );
