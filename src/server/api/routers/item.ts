6516e9fe4f99b96e6cc8ef5fe39b7c07bfe8bf1c
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-11 17:51:22 +0300

    Fix moving multiple items

--- src/server/api/routers/item.ts
+++ src/server/api/routers/item.ts
@@ -1,53 +1,47 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const itemsRouter = createTRPCRouter({
-  getItem: protectedProcedure
-    .input(z.string())
-    .query(({ ctx, input }) => {
-      return ctx.prisma.item.findUnique({
-        where: { id: input },
-      });
-    }),
+  getItem: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.item.findUnique({
+      where: { id: input },
+    });
+  }),
 
   deleteItems: protectedProcedure
     .input(z.array(z.string()))
     .mutation(({ ctx, input }) => {
       return ctx.prisma.item.deleteMany({
-        where: {id: {in: input}}
+        where: { id: { in: input } },
       });
     }),
 
   // Only from one list to another
   // Since it disconnects the lists by the listId of the first item
   moveItems: protectedProcedure
-    .input(z.object({
-      targetListId: z.string(),
-      itemIds: z.array(z.string()).min(1),
-    }))
+    .input(
+      z.object({
+        targetListId: z.string(),
+        itemIds: z.array(z.string()).min(1),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-      const prevListIdOfFirstItem = (await ctx.prisma.item.findFirst({
-        where: {id: {in: input.itemIds}},
-        select: {lists: {select: {listId: true}}}
-      }))?.lists[0]?.listId;
-      
-      return ctx.prisma.item.update({
-        where: {id: input.itemIds[0]},
+      const prevListIdOfFirstItem = (
+        await ctx.prisma.item.findFirst({
+          where: { id: { in: input.itemIds } },
+          select: { lists: { select: { listId: true } } },
+        })
+      )?.lists[0]?.listId;
+
+      return ctx.prisma.itemsInLists.updateMany({
+        where: {
+          AND: { itemId: { in: input.itemIds }, listId: prevListIdOfFirstItem },
+        },
         data: {
-          lists: {
-            create: {
-              assignedBy: {connect: {id: ctx.session.user.id}},
-              list: {connect: {id: input.targetListId}}
-            },
-            deleteMany: {
-              listId: {in: prevListIdOfFirstItem}
-            }
-          }
-        }
+          listId: input.targetListId,
+          assignedAt: new Date(),
+        },
       });
     }),
 });
