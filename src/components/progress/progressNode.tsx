eb984debc23e5b5f24c303362ef43631e55f914a
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-17 00:20:20 +0300

    Expand progressNode dictionary with editors

--- src/components/progress/progressNode.tsx
+++ src/components/progress/progressNode.tsx
@@ -27,19 +27,19 @@ export function ProgressNode({
       onMouseEnter={() => setIsEditing(true)}
       onMouseLeave={() => setIsEditing(false)}
     >
-      {isEditing ? (
-        <Slider
-          className="w-24 rounded border-[1px] border-slate-700"
-          value={[value]}
-          onValueChange={(newValue) => setValue(newValue[0] ?? 0)}
-          min={0}
-          max={progress.maxValue}
-          onValueCommit={() => updateProgress({ itemId, newProgress: value })}
-        />
-      ) : (
-        progressTypeToDisplay[progress.type as ProgressType]?.({ progress }) ??
-        defaultDisplay({ progress })
-      )}
+      {!isEditing
+        ? progressTypeToDisplay[progress.type as ProgressType]?.display({
+            progress,
+            value,
+          }) ?? defaultDisplay({ progress })
+        : progressTypeToDisplay[progress.type as ProgressType]?.editor({
+            value,
+            progress,
+            onValueChange: (newValue) => setValue(newValue),
+            onValueCommit: (newValue) => {
+              return updateProgress({ itemId, newProgress: newValue });
+            },
+          }) ?? defaultDisplay({ progress })}
     </div>
   );
 }
@@ -54,14 +54,48 @@ const defaultDisplay = ({ progress }: { progress: Progress }) => {
 };
 
 const progressTypeToDisplay: {
-  [key in ProgressType]?: ({ progress }: { progress: Progress }) => ReactNode;
+  [key in ProgressType]?: {
+    display: (props: { progress: Progress; value: number }) => ReactNode;
+    editor: (props: {
+      value: number;
+      progress: Progress;
+      onValueChange: (newValue: number) => void;
+      onValueCommit: (newValue: number) => void;
+    }) => ReactNode;
+  };
 } = {
-  [ProgressType.CHECK]: ({ progress }) =>
-    progress.currentValue > 0 ? "DONE" : "NOT DONE",
-  [ProgressType.SLIDER]: ({ progress }) => (
-    <ProgressBar
-      value={(progress.currentValue / progress.maxValue) * 100}
-      className="w-24 border-[1px] border-slate-700"
-    />
-  ),
+  [ProgressType.CHECK]: {
+    display: ({ value }) => (
+      <input className="h-6 w-6" type="checkbox" checked={value === 1} readOnly />
+    ),
+    editor: ({ value, onValueChange, onValueCommit }) => (
+      <input
+        className="h-6 w-6"
+        type="checkbox"
+        checked={value === 1}
+        onChange={(e) => {
+          onValueChange(e.target.checked ? 1 : 0);
+          onValueCommit(e.target.checked ? 1 : 0);
+        }}
+      />
+    ),
+  },
+  [ProgressType.SLIDER]: {
+    display: ({ progress }) => (
+      <ProgressBar
+        value={(progress.currentValue / progress.maxValue) * 100}
+        className="w-24 border-[1px] border-slate-700"
+      />
+    ),
+    editor: ({ value, progress, onValueChange, onValueCommit }) => (
+      <Slider
+        className="w-24 rounded border-[1px] border-slate-100"
+        value={[value]}
+        onValueChange={(newValue) => onValueChange(newValue[0] ?? 0)}
+        min={0}
+        max={progress.maxValue}
+        onValueCommit={(newValue) => onValueCommit(newValue[0] ?? 0)}
+      />
+    ),
+  },
 };
--- src/server/api/routers/item.ts
+++ src/server/api/routers/item.ts
@@ -161,6 +161,9 @@ export const itemsRouter = createTRPCRouter({
             },
           },
         },
+        include: {
+          progress: true,
+        },
       });
     }),
 });
