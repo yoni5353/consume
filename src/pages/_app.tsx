04d66c6aa4c4d8069272933570488716a82b0ffc
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-04-25 10:45:11 +0300

    Use new shadcn/ui theming config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,29 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  useEffect(() => {
+    // Apply to whole body, including popovers
+    document.querySelector("body")?.classList.add("dark");
+  }, []);
+
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider>
-        <ReactQueryDevtools />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <ThemeProvider>
+          <ReactQueryDevtools />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </>
   );
 };
 
