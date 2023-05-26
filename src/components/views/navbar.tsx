9fde3f82339e92bebbfb43b487bfb14857e64ddf
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-05-26 23:41:10 +0300

    Move sidebar to new navbar component

---
+++ src/components/views/navbar.tsx
@@ -0,0 +1,154 @@
+import { BikeIcon, ListIcon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+import { api } from "~/utils/api";
+import { ContextMenu, ContextMenuTrigger } from "~/components/ui/context-menu";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { ListContextMenu } from "~/components/listContextMenu";
+import { Consume } from "~/components/ui/con-sume";
+
+export function NavBar() {
+  const [currentContextMenuSprint, setCurrentContextMenuSprint] = useState<string>();
+  const [currentContextMenuList, setCurrentContextMenuList] = useState<string>();
+  const [sprintsRef] = useAutoAnimate<HTMLDivElement>();
+  const [backlogRef] = useAutoAnimate<HTMLDivElement>();
+  const [selectedGoal, setSelectedGoal] = useState<string>();
+
+  const { data: lists } = api.lists.getBacklog.useQuery();
+
+  const { data: sprints } = api.lists.getSprints.useQuery();
+
+  // const { mutate: deleteGoal } = api.goals.delete.useMutation({
+  //   onSuccess: () => {
+  //     void ctx.goals.getGoals.invalidate();
+  //   },
+  // });
+
+  return (
+    <aside className="sidebar space-y-5 pr-10">
+      <div className="space-y-12 overflow-auto px-2">
+        <div>
+          <h2 className="align-center mb-2 flex flex-row px-2 text-lg font-semibold tracking-tight">
+            Sprints
+          </h2>
+          <div className="space-y-2 pb-1">
+            <ContextMenu modal={false}>
+              <ContextMenuTrigger>
+                <div className="sprints-list space-y-2" ref={sprintsRef}>
+                  {sprints?.map((sprint) => (
+                    <Button
+                      key={sprint.id}
+                      // variant={currentLists.includes(sprint.id) ? "secondary" : "ghost"}
+                      variant="ghost"
+                      size="sm"
+                      className="w-full justify-start text-xs font-extrabold"
+                      onClick={() => {
+                        // selectSprint(sprint.id);
+                        setCurrentContextMenuSprint(sprint.id);
+                      }}
+                      onAuxClick={() => {
+                        // selectSprint(sprint.id); // do not fly to sprint here
+                        setCurrentContextMenuSprint(sprint.id);
+                      }}
+                    >
+                      <BikeIcon className="mr-2 h-4 w-4" />
+                      <span className="uppercase">{sprint.title}</span>
+                    </Button>
+                  ))}
+                </div>
+              </ContextMenuTrigger>
+              <ListContextMenu
+                selectedListId={currentContextMenuSprint ?? null}
+                isSprint
+              />
+            </ContextMenu>
+          </div>
+        </div>
+        <div>
+          <h2 className="align-center mb-2 flex flex-row px-2 text-lg font-semibold tracking-tight">
+            Backlog
+          </h2>
+          <div className="space-y-2 pb-1">
+            <ContextMenu modal={false}>
+              <ContextMenuTrigger>
+                <div className="lists-list space-y-2" ref={backlogRef}>
+                  {lists?.map((list) => (
+                    <Button
+                      key={list.id}
+                      // variant={currentLists.includes(list.id) ? "secondary" : "ghost"}
+                      variant="ghost"
+                      size="sm"
+                      className="w-full justify-start text-xs font-extrabold"
+                      onClick={() => {
+                        setCurrentContextMenuList(list.id);
+                        // moveToList(list.id)
+                      }}
+                      onAuxClick={() => {
+                        setCurrentContextMenuList(list.id);
+                      }}
+                    >
+                      <ListIcon className="mr-2 h-4 w-4" />
+                      <span className="uppercase">{list.title}</span>
+                    </Button>
+                  ))}
+                </div>
+              </ContextMenuTrigger>
+              <ListContextMenu selectedListId={currentContextMenuList ?? null} />
+            </ContextMenu>
+          </div>
+        </div>
+        <div>
+          {/* <h2 className="mb-2 flex flex-row items-center px-2 text-lg font-semibold tracking-tight">
+            Goals
+          </h2>
+          <div className="space-y-2 pb-1">
+            <ContextMenu modal={false}>
+              <ContextMenuTrigger>
+                <div className="lists-list space-y-2" ref={backlogRef}>
+                  {goals?.map((goal) => (
+                    <Button
+                      key={goal.id}
+                      variant="ghost"
+                      size="sm"
+                      className="flex w-full flex-row justify-start space-x-2 text-xs font-extrabold"
+                      onClick={() => setSelectedGoal(goal.id)}
+                      onAuxClick={() => setSelectedGoal(goal.id)}
+                    >
+                      <span className="uppercase">1/{goal.targetValue}</span>
+                      <div className="flex items-center space-x-2 rounded-lg bg-secondary/70 p-1">
+                        <MediaTypeIcon
+                          mediaType={goal.targetMediaType ?? undefined}
+                          className="mr-2"
+                        />
+                        {goal.targetMediaType?.name ?? "Item"}
+                      </div>
+                    </Button>
+                  ))}
+                </div>
+              </ContextMenuTrigger>
+              <ContextMenuContent>
+                <ContextMenuItem
+                  onSelect={() => {
+                    selectedGoal && deleteGoal(selectedGoal);
+                  }}
+                >
+                  <Trash2 className="mr-2 h-4 w-4" />
+                  Delete Goal
+                </ContextMenuItem>
+              </ContextMenuContent>
+            </ContextMenu>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="w-full justify-start"
+              onClick={() => setIsCreatingGoal(true)}
+            >
+              <PlusCircleIcon className="mr-2 h-4 w-4" />
+              Set Goal
+            </Button>
+          </div> */}
+        </div>
+      </div>
+    </aside>
+  );
+}
--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,37 +1,27 @@
-import { BikeIcon, Layout, ListIcon, PlusCircleIcon, Trash2 } from "lucide-react";
+import { ArrowBigRight, Layout, PlusCircleIcon } from "lucide-react";
 import { type NextPage } from "next";
 import { useCallback, useState } from "react";
 import { ItemsList } from "~/components/itemsList";
 import { CreateListDialog } from "~/components/createListDialog";
 import { Button } from "~/components/ui/button";
 import { api } from "~/utils/api";
-import {
-  ContextMenu,
-  ContextMenuItem,
-  ContextMenuTrigger,
-  ContextMenuContent,
-} from "~/components/ui/context-menu";
 import { type CreateListSechemaType } from "~/utils/apischemas";
 import { cn } from "~/utils/ui/cn";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { Toggle } from "~/components/ui/toggle";
 import { ItemCreationInput } from "~/components/itemCreationInput";
-import { ListContextMenu } from "~/components/listContextMenu";
 import { CreateGoalDialog } from "~/components/createGoalDialog";
-import { MediaTypeIcon } from "~/components/resources/mediaTypeIcon";
 import { Consume } from "~/components/ui/con-sume";
+import { NavBar } from "~/components/views/navbar";
 
 const BoardPage: NextPage = () => {
   const [currentLayout, setCurrentLayout] = useState<"list" | "grid">("list");
   const [currentLists, setCurrentLists] = useState<string[]>([]);
-  const [currentContextMenuSprint, setCurrentContextMenuSprint] = useState<string>();
   const [selectedItemId, setSelectedItemId] = useState<string>();
-  const [sprintsRef] = useAutoAnimate<HTMLDivElement>();
-  const [backlogRef] = useAutoAnimate<HTMLDivElement>();
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const [sprintsViewRef] = useAutoAnimate<HTMLDivElement>();
   const [currentCreationList, setCurrentCreationList] = useState<string>();
   const [isCreatingGoal, setIsCreatingGoal] = useState<boolean>();
-  const [selectedGoal, setSelectedGoal] = useState<string>();
 
   const ctx = api.useContext();
 
@@ -47,14 +37,6 @@ const BoardPage: NextPage = () => {
       },
     }
   );
-
-  const { data: goals } = api.goals.getGoals.useQuery();
-
-  const { mutate: deleteGoal } = api.goals.delete.useMutation({
-    onSuccess: () => {
-      void ctx.goals.getGoals.invalidate();
-    },
-  });
 
   const { mutate: createList } = api.lists.createList.useMutation({
     onSuccess: (newList) => {
@@ -105,156 +87,17 @@ const BoardPage: NextPage = () => {
   return (
     <>
       <main className="flex min-h-screen flex-row bg-gradient-to-t from-blue-500 to-[#76b9ce]">
-        <div className="main-grid m-3 grid w-full grid-cols-4 overflow-hidden rounded-md bg-background p-5 xl:grid-cols-5 ">
-          <aside className="sidebar space-y-5 pr-10">
-            <h1 className="align-center mb-2 flex flex-row px-2 text-2xl font-extrabold tracking-tight">
-              <Consume />
-            </h1>
-            <div className="space-y-12 overflow-auto px-2">
-              <div>
-                <h2 className="align-center mb-2 flex flex-row px-2 text-lg font-semibold tracking-tight">
-                  Sprints
-                </h2>
-                <div className="space-y-2 pb-1">
-                  <ContextMenu modal={false}>
-                    <ContextMenuTrigger>
-                      <div className="sprints-list space-y-2" ref={sprintsRef}>
-                        {sprints?.map((sprint) => (
-                          <Button
-                            key={sprint.id}
-                            variant={
-                              currentLists.includes(sprint.id) ? "secondary" : "ghost"
-                            }
-                            size="sm"
-                            className="w-full justify-start text-xs font-extrabold"
-                            onClick={() => {
-                              selectSprint(sprint.id);
-                              setCurrentContextMenuSprint(sprint.id);
-                            }}
-                            onAuxClick={() => {
-                              selectSprint(sprint.id); // do not fly to sprint here
-                              setCurrentContextMenuSprint(sprint.id);
-                            }}
-                          >
-                            <BikeIcon className="mr-2 h-4 w-4" />
-                            <span className="uppercase">{sprint.title}</span>
-                          </Button>
-                        ))}
-                        <Button
-                          variant="ghost"
-                          size="sm"
-                          className="w-full justify-start"
-                          onClick={() => openListCreation({ isSprint: true })}
-                        >
-                          <PlusCircleIcon className="mr-2 h-4 w-4" />
-                          New Sprint
-                        </Button>
-                      </div>
-                    </ContextMenuTrigger>
-                    <ListContextMenu
-                      selectedListId={currentContextMenuSprint ?? null}
-                      isSprint
-                    />
-                  </ContextMenu>
-                </div>
-              </div>
-              <div>
-                <h2 className="align-center mb-2 flex flex-row px-2 text-lg font-semibold tracking-tight">
-                  Backlog
-                </h2>
-                <div className="space-y-2 pb-1">
-                  <ContextMenu modal={false}>
-                    <ContextMenuTrigger>
-                      <div className="lists-list space-y-2" ref={backlogRef}>
-                        {lists?.map((list) => (
-                          <Button
-                            key={list.id}
-                            variant={
-                              currentLists.includes(list.id) ? "secondary" : "ghost"
-                            }
-                            size="sm"
-                            className="w-full justify-start text-xs font-extrabold"
-                            onClick={() => moveToList(list.id)}
-                            onAuxClick={() => moveToList(list.id)}
-                          >
-                            <ListIcon className="mr-2 h-4 w-4" />
-                            <span className="uppercase">{list.title}</span>
-                          </Button>
-                        ))}
-                      </div>
-                    </ContextMenuTrigger>
-                    <ListContextMenu
-                      selectedListId={currentLists[0] ?? null}
-                      onDelete={() => {
-                        moveToList(lists?.[0]?.id);
-                      }}
-                    />
-                  </ContextMenu>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    className="w-full justify-start"
-                    onClick={() => openListCreation()}
-                  >
-                    <PlusCircleIcon className="mr-2 h-4 w-4" />
-                    Create List
-                  </Button>
-                </div>
-              </div>
-              <div>
-                <h2 className="mb-2 flex flex-row items-center px-2 text-lg font-semibold tracking-tight">
-                  Goals
-                </h2>
-                <div className="space-y-2 pb-1">
-                  <ContextMenu modal={false}>
-                    <ContextMenuTrigger>
-                      <div className="lists-list space-y-2" ref={backlogRef}>
-                        {goals?.map((goal) => (
-                          <Button
-                            key={goal.id}
-                            variant="ghost"
-                            size="sm"
-                            className="flex w-full flex-row justify-start space-x-2 text-xs font-extrabold"
-                            onClick={() => setSelectedGoal(goal.id)}
-                            onAuxClick={() => setSelectedGoal(goal.id)}
-                          >
-                            <span className="uppercase">1/{goal.targetValue}</span>
-                            <div className="flex items-center space-x-2 rounded-lg bg-secondary/70 p-1">
-                              <MediaTypeIcon
-                                mediaType={goal.targetMediaType ?? undefined}
-                                className="mr-2"
-                              />
-                              {goal.targetMediaType?.name ?? "Item"}
-                            </div>
-                          </Button>
-                        ))}
-                      </div>
-                    </ContextMenuTrigger>
-                    <ContextMenuContent>
-                      <ContextMenuItem
-                        onSelect={() => {
-                          selectedGoal && deleteGoal(selectedGoal);
-                        }}
-                      >
-                        <Trash2 className="mr-2 h-4 w-4" />
-                        Delete Goal
-                      </ContextMenuItem>
-                    </ContextMenuContent>
-                  </ContextMenu>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    className="w-full justify-start"
-                    onClick={() => setIsCreatingGoal(true)}
-                  >
-                    <PlusCircleIcon className="mr-2 h-4 w-4" />
-                    Set Goal
-                  </Button>
-                </div>
-              </div>
+        <div className="main-grid m-3 grid w-full grid-cols-5 overflow-hidden rounded-md bg-background p-5 xl:grid-cols-6">
+          <div className="flex flex-col justify-center">
+            <div className="flex flex-row items-center">
+              {isNavbarOpen && <NavBar />}
+              <Toggle onPressedChange={(pressed) => setIsNavbarOpen(pressed)}>
+                <ArrowBigRight className="h-4 w-4" />
+              </Toggle>
             </div>
-          </aside>
-          <div className="col-span-3 overflow-hidden border-l border-slate-500 xl:col-span-4">
+          </div>
+
+          <div className="col-span-3 overflow-hidden xl:col-span-4">
             <div className="flex w-full flex-row justify-between">
               <div />
               <Consume className="text-2xl" />
