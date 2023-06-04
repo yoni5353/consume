fa53baee4050771ca3afdb427f27520421f5d2ba
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-06-05 00:05:15 +0300

    Turn Consume component to Next Link

--- src/components/ui/con-sume.tsx
+++ src/components/ui/con-sume.tsx
@@ -1,14 +1,17 @@
+import Link from "next/link";
+
 export function Consume({ className }: { className?: string }) {
   return (
     <>
-      <span
+      <Link
+        href="/"
         className={
           "bg-gradient-to-br from-blue-300 to-[hsl(202,62%,80%)] bg-clip-text font-extrabold text-transparent " +
           className
         }
       >
         CONSUME
-      </span>
+      </Link>
     </>
   );
 }
