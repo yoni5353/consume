aef6e3c85e5697c214a1b0cfcd90f7dde6034a83
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-06-04 22:58:54 +0300

    Add static Avatar to topbar

--- src/components/views/topbar.tsx
+++ src/components/views/topbar.tsx
@@ -1,6 +1,12 @@
-import { Layout } from "lucide-react";
+import { Layout, UserIcon } from "lucide-react";
 import { Consume } from "../ui/con-sume";
 import { Toggle } from "../ui/toggle";
+import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export function TopBar({
   onLayoutChange,
@@ -11,7 +17,20 @@ export function TopBar({
     <div className="flex w-full flex-row items-center justify-between p-2">
       <div />
       <Consume className="text-2xl" />
-      <div className="flex flex-row-reverse">
+      <div className="mr-1 flex flex-row-reverse gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="https://github.com/yoni5353.png" alt="@yonisku" />
+              <AvatarFallback>
+                <UserIcon />
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end">
+            Content
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Toggle onPressedChange={(pressed) => onLayoutChange(pressed ? "grid" : "list")}>
           <Layout className="h-4 w-4" />
         </Toggle>
