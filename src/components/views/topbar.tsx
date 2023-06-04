087fb2b99dddab60398a542e570d2333baf9bc42
Yonatan Skourjinsky <yoni.sku@gmail.com>
2023-06-04 23:03:56 +0300

    Add mock content to topbar avatar

--- src/components/views/topbar.tsx
+++ src/components/views/topbar.tsx
@@ -1,18 +1,37 @@
-import { Layout, UserIcon } from "lucide-react";
+import {
+  CreditCard,
+  Layout,
+  LogOut,
+  PlusCircle,
+  Settings,
+  User,
+  UserIcon,
+} from "lucide-react";
 import { Consume } from "../ui/con-sume";
 import { Toggle } from "../ui/toggle";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { useSession } from "next-auth/react";
 
 export function TopBar({
   onLayoutChange,
 }: {
   onLayoutChange: (layout: "grid" | "list") => void;
 }) {
+  const { data: sessionData } = useSession();
+
+  const userName = sessionData?.user?.name;
+  const userEmail = sessionData?.user?.email;
+
   return (
     <div className="flex w-full flex-row items-center justify-between p-2">
       <div />
@@ -28,7 +47,31 @@ export function TopBar({
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-56" align="end">
-            Content
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">{userName}</p>
+                <p className="text-xs leading-none text-muted-foreground">{userEmail}</p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <DropdownMenuShortcut>⇧P</DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <DropdownMenuShortcut>⇧S</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Log out</span>
+              <DropdownMenuShortcut>⇧Q</DropdownMenuShortcut>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
         <Toggle onPressedChange={(pressed) => onLayoutChange(pressed ? "grid" : "list")}>
